37ec250e7bf8bc8fbc3d3bb372ee5fb5f30082e7
Carlos Reyes <careyespe26@gmail.com>
2023-01-29 18:51:38 -0500

    ejercicio morral y lectura texto

---
+++ manejo_archivos/leer_optimamente.py
@@ -0,0 +1,14 @@
+with open('manejo_archivos/texto.txt') as text:
+    print(text.read())
+
+#escribir en archivo
+
+
+#Sobre escribir archivo con 'w'
+#with open('manejo_archivos/texto.txt', 'w') as text:
+    #text.write('iwi')
+
+#Agregando informacion al archivo con 'a'
+#with open('manejo_archivos/texto.txt', 'a') as text:
+    #text.write('iwi\n')
+
