a83ca12b5b04eb24683207e9ce7ee755fcb9bf81
Carlos Reyes <careyespe26@gmail.com>
2023-01-29 18:52:12 -0500

    lectura de textos y ejercicio morral

---
+++ manejo_archivos/archivo_txt.py
@@ -0,0 +1,13 @@
+archivo = open('./manejo_archivos/texto.txt')
+
+#archivo entero
+#lectura= archivo.read()
+
+#lineas
+#linea_1 = archivo.readline()
+#lineas = archivo.readlines()
+
+#cerrar archivo
+#archivo.close()
+print()
+
