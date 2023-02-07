29ab72ae66a36819d59c293bd5d3a496f5630768
Carlos Reyes <careyespe26@gmail.com>
2023-02-07 00:33:50 -0500

    estadistica

--- estadistica_computacional/estadisticas.py
+++ estadistica_computacional/estadisticas.py
@@ -1,12 +1,31 @@
 import random
+import math
 
 def media(X):
     return sum(X) / len(X)
 
 
-if __name__ == '__main__':
-    X = [random.randint(1, 21) for i in range(20)]
+def varianza(X):
     mu = media(X)
+    
+    acumulador = 0
+    for x in X:
+        acumulador += (x - mu)**2
+    
+    return acumulador / len(X)
 
-    print(X)
-    print(mu)
+
+def desviacion_estandar(X):
+   return math.sqrt(varianza(X)) 
+
+
+if __name__ == '__main__':
+    X = [random.randint(9, 12) for i in range(20)]
+    mu = media(X)
+    Varianza = varianza(X)
+    sigma = desviacion_estandar(X)
+
+    print(f'Arreglo X: {X}')
+    print(f'Media : {mu}')
+    print(f'Varianza: {Varianza}')
+    print(f'Desviacion estandar: {sigma}')
